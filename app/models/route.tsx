f5bab4e343a7c1c277fced2b167fef32b09e2d95
MarkoV <marek.kaszewski@gmail.com>
2024-09-11 10:43:18 +0100

    adopting layout to rest api mode

---
+++ app/models/route.tsx
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+const data = {
+    name: "MODEL",
+    email: "john.doe@example.com",
+    age: 30,
+    gender: "male",
+    phone: "1234567890",
+    address: "123 Main St, Anytown, USA",
+    city: "Anytown",
+    state: "CA",
+}
+
+export const GET = async () => {
+    return new NextResponse(JSON.stringify(data), { status: 200 });
+};
