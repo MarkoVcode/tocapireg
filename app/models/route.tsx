e886475a43f770d9bc97a0ef3ed48165a40f6835
MarkoV <marek.kaszewski@gmail.com>
2024-09-13 16:18:44 +0100

    basic app api + db permission

--- app/models/route.tsx
+++ app/models/route.tsx
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { scanTable } from "@/lib/db";
 
+
 export const GET = async () => {
     const data = await scanTable();
     return new NextResponse(JSON.stringify(data), { 
