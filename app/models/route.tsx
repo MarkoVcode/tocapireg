0b5dc869314b67bf1de79d3d61991690f5d1904f
MarkoV <marek.kaszewski@gmail.com>
2024-09-17 12:44:59 +0100

    disabling prerender

--- app/models/route.tsx
+++ app/models/route.tsx
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { scanTable } from "@/lib/db";
 
-
-export const GET = async () => {
+export const GET = async (request: Request) => {
+    console.log("request", request.headers);
     const data = await scanTable();
     return new NextResponse(JSON.stringify(data), { 
         status: 200,
