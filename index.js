7a024a6829aecde93ddd634ee57d4fde1808db6c
MarkoV <marek.kaszewski@gmail.com>
2024-09-19 12:10:25 +0100

    Making the api functional

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+const env = require('./lib/env');
+env.processEnvironment();
 const express = require("express");
 const serverless = require("serverless-http");
 const dblib = require('./lib/db');
-const env = require('./lib/env');
 
-env.processEnvironment();
 const app = express();
 
 app.use(express.json());
 
 app.get("/models/:modelId", async function (req, res) {
-  const data = await dblib.queryItemByIndex('modelId', 'id = :modelId', {':modelId': req.params.modelId});
-  res.json( data );
+  const data = await dblib.queryItemByIndex('modelId', 'id = :modelId', { ':modelId': req.params.modelId });
+  res.json(data);
 });
 
 app.get("/models", async function (req, res) {
   const data = await dblib.scanTable();
-  res.json( data );
+  res.json(data);
 });
 
 app.get("/version", async function (req, res) {
@@ -25,21 +25,40 @@ app.get("/version", async function (req, res) {
     infrastructure: process.env.DEPLOYMENT === 'infrastructure',
     dbRegion: process.env.DBREGION || ""
   }
-  res.json( data );
+  res.json(data);
 });
 
+// Return the unique available model ID 
 app.get("/registry", async function (req, res) {
   const data = { modelId: "4fe2" };
-  res.json( data );
+  res.json(data);
 });
 
+// Add models to Registry
 app.post("/registry", async function (req, res) {
   const { modelsRepoUrl } = req.body;
   if (typeof modelsRepoUrl !== "string") {
     res.status(400).json({ error: '"modelsRepoUrl" must be a string' });
-  } 
-
-  res.status(500).json({ error: "Could not create user" });
+  }
+  const response = await fetch(modelsRepoUrl + '/models');
+  const models = await response.json();
+  const modelsListNew = [];
+  for (const model of models) {
+    await dblib.addItem(model); 
+    modelsListNew.push(model.id);
+  }
+  const modelsx = await dblib.queryItemsByServiceUrl(modelsRepoUrl);
+  let deletedCounter = 0;
+  if (modelsx) {
+    for (const modelx of modelsx) {
+      if (!modelsListNew.includes(modelx.id)) {
+        await dblib.deleteItem(modelx.id, modelsRepoUrl);
+        deletedCounter++;
+      }
+    }
+  }
+  const responseBody = { modelsUpdated: models.length, modelsDeleted: deletedCounter, status: 'success' };
+  res.json(responseBody);
 });
 
 app.use((req, res, next) => {
